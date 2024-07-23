c719da3428f58cbc688cf4566df4cf399c834ee2
DML-0205Michael <libramichael6@gmail.com>
2024-07-23 01:37:53 -0400

    add costmap_converter. reset TEB params and disable enable_multithreading

--- catkin_ws/src/slam_pkg/carto_config/my_pure_localization.lua
+++ catkin_ws/src/slam_pkg/carto_config/my_pure_localization.lua
@@ -12,13 +12,66 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
--- include "my_backpack_2d.lua"
-include "my_carto_2d_SLAM.lua"
+
+include "map_builder.lua"
+include "trajectory_builder.lua"
+
+-- TRAJECTORY_BUILDER.pure_localization = true
+
+options = {
+  map_builder = MAP_BUILDER,
+  trajectory_builder = TRAJECTORY_BUILDER,
+  map_frame = "map",
+  tracking_frame = "base_link",
+  published_frame = "odom",
+  odom_frame = "odom",
+  provide_odom_frame = false,
+  publish_frame_projected_to_2d = false,
+  -- use_pose_extrapolator = true,
+  use_odometry = true,
+  use_nav_sat = false,
+  use_landmarks = false,
+  num_laser_scans = 1,
+  num_multi_echo_laser_scans = 0,
+  num_subdivisions_per_laser_scan = 1,
+  num_point_clouds = 0,
+  lookup_transform_timeout_sec = 0.2,
+  submap_publish_period_sec = 0.3,
+  pose_publish_period_sec = 5e-3,
+  trajectory_publish_period_sec = 30e-3,
+  rangefinder_sampling_ratio = 1.,
+  odometry_sampling_ratio = 1.,
+  fixed_frame_pose_sampling_ratio = 1.,
+  imu_sampling_ratio = 1.,
+  landmarks_sampling_ratio = 1.,
+}
+
+MAP_BUILDER.use_trajectory_builder_2d = true
+
 
 TRAJECTORY_BUILDER.pure_localization_trimmer = {
   max_submaps_to_keep = 3,
 }
-POSE_GRAPH.optimize_every_n_nodes = 5
--- TRAJECTORY_BUILDER.pure_localization = true
+POSE_GRAPH.optimize_every_n_nodes = 10
+
+--TRAJECTORY_BUILDER_2D.submaps.num_range_data = 35
+TRAJECTORY_BUILDER_2D.min_range = 0.3
+TRAJECTORY_BUILDER_2D.max_range = 8.
+--TRAJECTORY_BUILDER_2D.missing_data_ray_length = 1.
+TRAJECTORY_BUILDER_2D.use_imu_data = false
+
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = false
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.occupied_space_weight = 1.
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 1.
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 1.
+
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 80.
+TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.resolution = 0.03
+
+POSE_GRAPH.optimize_every_n_nodes = 160.
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.3
+POSE_GRAPH.constraint_builder.max_constraint_distance = 15.
+POSE_GRAPH.constraint_builder.min_score = 0.48
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.60
 
 return options
